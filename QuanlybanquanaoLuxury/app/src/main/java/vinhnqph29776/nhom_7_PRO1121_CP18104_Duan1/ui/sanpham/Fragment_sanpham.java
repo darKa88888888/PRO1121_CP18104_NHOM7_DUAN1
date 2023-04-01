44b15f75c6b8d2b499e1d7c285cbf0acf651d751
darKa88888888 <127387518+darKa88888888@users.noreply.github.com>
2023-04-01 14:49:15 +0700

    ListDsCrud

--- QuanlybanquanaoLuxury/app/src/main/java/vinhnqph29776/nhom_7_PRO1121_CP18104_Duan1/ui/sanpham/Fragment_sanpham.java
+++ QuanlybanquanaoLuxury/app/src/main/java/vinhnqph29776/nhom_7_PRO1121_CP18104_Duan1/ui/sanpham/Fragment_sanpham.java
@@ -4,26 +4,44 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.R;
 
 import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.databinding.FragmentSanphamBinding;
 import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.Frag_quan.Frag_ao;
+import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.Frag_quan.Frag_giay;
 import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.Frag_quan.Frag_quan;
+import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.rcv.cate_ADT;
+import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.rcv.category;
 
-public class Fragment_sanpham extends Fragment {
+public class Fragment_sanpham extends Fragment implements OnItemCLickListener{
     FragmentManager fm;
-    ImageView img_ao,img_quan,img_shop;
-    Button btn_ao,btn_quan;
+    ImageView img_shop;
     private FragmentSanphamBinding binding;
+    RecyclerView recyclerView;
+    cate_ADT adt;
+    ArrayList<category> list;
+
+    CardView cv01,cv02,cv03;
+
+
+    private OnItemCLickListener lickListener;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -33,34 +51,32 @@ public class Fragment_sanpham extends Fragment {
         binding = FragmentSanphamBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
+
         return root;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        img_ao=view.findViewById(R.id.img_ao);
-        img_quan=view.findViewById(R.id.img_q);
         img_shop=view.findViewById(R.id.img_shop);
-        btn_ao=view.findViewById(R.id.btn_ao);
-        btn_quan=view.findViewById(R.id.btn_quan);
+        cv01=view.findViewById(R.id.cv_ao);
+        cv02=view.findViewById(R.id.cv_q);
+        cv03=view.findViewById(R.id.cv_giay);
         fm= getActivity().getSupportFragmentManager();
         Frag_quan frag_quan=new Frag_quan();
         Frag_ao frag_ao=new Frag_ao();
-        fm.beginTransaction().add(R.id.frag_ctn_sp,frag_quan).commit();
-        btn_quan.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fm.beginTransaction().replace(R.id.frag_ctn_sp,frag_quan).commit();
-            }
-        });
+        Frag_giay frag_giay=new Frag_giay();
+        img_shop=view.findViewById(R.id.img_shop);
+////        recyclerView=view.findViewById(R.id.rcv_homefrag);
+//
+//        adt=new cate_ADT(list,this);
+//        LinearLayoutManager layoutManager=new LinearLayoutManager(getContext(), RecyclerView.HORIZONTAL,false);
+//
+//        recyclerView.setLayoutManager(layoutManager);
+//        adt.setData(getList());
+//        recyclerView.setAdapter(adt);
 
-        btn_ao.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                fm.beginTransaction().replace(R.id.frag_ctn_sp,frag_ao).commit();
-            }
-        });
+        fm.beginTransaction().add(R.id.frag_ctn_sp,frag_ao).commit();
 
         img_shop.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -68,11 +84,46 @@ public class Fragment_sanpham extends Fragment {
                 Toast.makeText(getContext(), "Helllo", Toast.LENGTH_SHORT).show();
             }
         });
+
+        cv01.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm.beginTransaction().replace(R.id.frag_ctn_sp,frag_ao).commit();
+            }
+        });
+        cv02.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm.beginTransaction().replace(R.id.frag_ctn_sp,frag_quan).commit();
+            }
+        });
+
+        cv03.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                fm.beginTransaction().replace(R.id.frag_ctn_sp,frag_giay).commit();
+            }
+        });
+
+
     }
 
     @Override
     public void onDestroyView() {
         super.onDestroyView();
         binding = null;
+    }
+
+    public List<category> getList() {
+        List<category> categoryList=new ArrayList<>();
+        categoryList.add(new category(R.drawable.mlb_ao,"ÁO"));
+        categoryList.add(new category(R.drawable.mlb_q,"Quần"));
+        categoryList.add(new category(R.drawable.img,"Giày"));
+        return categoryList;
+    }
+
+    @Override
+    public void OnItemCLickListener(category category) {
+
     }
 }
---
+++ QuanlybanquanaoLuxury/app/src/main/java/vinhnqph29776/nhom_7_PRO1121_CP18104_Duan1/ui/sanpham/OnItemCLickListener.java
@@ -0,0 +1,9 @@
+package vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham;
+
+import android.view.View;
+
+import vinhnqph29776.nhom_7_PRO1121_CP18104_Duan1.ui.sanpham.rcv.category;
+
+public interface OnItemCLickListener {
+      void OnItemCLickListener(category category);
+}
